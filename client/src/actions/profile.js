e88b1ea7e806d8252cc224fb7d50f1dd1512e944
cybergeek7 <fahad.altuhafi@gmail.com>
2020-12-31 14:05:05 -0800

    Ceate Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,16 +4,46 @@ import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 // Get current user's profile
-export const getCurrentProfile = () => async (dispacth) => {
+export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await api.get('/profile/me');
 
-    dispacth({
+    dispatch({
       type: GET_PROFILE,
       payload: res.data,
     });
   } catch (err) {
-    dispacth({
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Create or update prodile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const res = await api.post('/profile', formData);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? 'Profile updated' : 'Profile created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
     });
